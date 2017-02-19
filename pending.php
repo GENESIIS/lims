133fda3f98927cea465a944dccad502346b8d159
isharaseram <seramishara@gmail.com>
2017-02-19 22:51:32 +0530

    Update pending.php

--- pending.php
+++ pending.php
@@ -72,9 +72,9 @@ if (($user!="")) {
             <nav class="templatemo-top-nav col-lg-12 col-md-12">
               <ul class="text-uppercase">
                 <!--li><a href="" class="active">Admin panel</a></li-->
-                <li><a href="">Foot</a></li>
-                <li><a href="">Arm</a></li>
-                <li><a href="">Other </a></li>
+<!--                <li><a href="">Foot</a></li>-->
+<!--                <li><a href="">Arm</a></li>
+                <li><a href="">Other </a></li>-->
               </ul>  
             </nav> 
           </div>
